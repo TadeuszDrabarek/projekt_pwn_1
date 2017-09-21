f791aa978766637ab34871f7d59e135855fef538
tadeusz <tadeusz.drabarek@wp.pl>
2017-09-22 00:16:39 +0200

    Na koniec dnia: czwartek+ poprawka, dodanie widoku v_dates

--- przykladowe_zapytania.sql
+++ przykladowe_zapytania.sql
@@ -142,6 +142,17 @@ inner join t_stawki_nauczycieli sn on sn.id_dlugosci=p.id_dlugosci
 group by concat(n.imie,' ',n.nazwisko);
 
 
+-- Widok techniczny z datami 
+create or replace view v_dates as 
+select gen_date from 
+(select adddate('1970-01-01',t4*10000 + t3*1000 + t2*100 + t1*10 + t0) gen_date from
+ (select 0 t0 union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t0,
+ (select 0 t1 union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t1,
+ (select 0 t2 union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t2,
+ (select 0 t3 union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t3,
+ (select 0 t4 union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t4) v
+where gen_date between '2017-01-01' and '2111-12-31';
+
 -- Zapytanie uzupełnia tabelę z zajęciami w zadanym okresie dat
 insert into t_zajecia(data_zajec,id_planu,id_nauczyciela,czy_odbyte,czy_odrabiane)
 select v.gen_date, p.id_planu, p.id_nauczyciela,1,0
